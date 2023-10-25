c96c9d670ea655d7f33cf915e5f11eb2ee7ca3cc
Nir A <niralt1984@gmail.com>
2023-10-25 19:08:21 +0300

    Starting to add databases + tests + dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Add x and y and return result.
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Add x from y and return result.
+    :param x:
+    :param y:
+    :return:
+    """
+    return y - x
